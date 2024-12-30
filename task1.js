5759d5ccccdf07cce4a6c8311a0b643b0ed1d381
vikktorija4 <vikyljas@gmail.com>
2024-12-30 14:55:29 +0100

    Update task1.js

--- task1.js
+++ task1.js
@@ -5,3 +5,13 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+
+const userList = document.querySelector(".userList")
+fetch("https://jsonplaceholder.typicode.com/users")
+ .then((response) => response.json())
+.then((users) => {
+  users.forEach((user) => {
+    userList.insertAdjacentHTML("beforeend", '<li>${user.username}</li>}
+                                })
+})
